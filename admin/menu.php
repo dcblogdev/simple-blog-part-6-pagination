4796f016a1701829fe775fdb9506e5f9f66066ac
David Carr <dave@daveismyname.com>
2017-08-27 18:38:26 +0100

    Update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -1,5 +1,5 @@
 <h1>Blog</h1>
-<p>Logged in as <?=$_SESSION['username'];?>
+<p>Logged in as <?=$_SESSION['username'];?></p>
 <ul id='adminmenu'>
 	<li><a href='index.php'>Blog</a></li>
 	<li><a href='categories.php'>Categories</a></li>
